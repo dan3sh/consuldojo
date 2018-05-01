c406deedb9a83e0820f55bfdcb21674d738d3193
Davor Kapsa <davor.kapsa@gmail.com>
2018-05-01 14:51:08 +0200

    add teardown kv

--- main.go
+++ main.go
@@ -14,20 +14,17 @@ func main() {
 		log.Fatalf("new client: %v", err)
 	}
 
+	err = setupKV(c)
+	if err != nil {
+		log.Fatalf("setup: %v", err)
+	}
+	defer func() {
+		err = teardownKV(c)
+		if err != nil {
+			log.Fatalf("teardown: %v", err)
+		}
+	}()
 	kv := c.KV()
-
-	p1 := &api.KVPair{Key: "app/k1", Value: []byte("v1")}
-	p2 := &api.KVPair{Key: "app/k2", Value: []byte("v2")}
-
-	_, err = kv.Put(p1, nil)
-	if err != nil {
-		log.Fatalf("put %v: %v", p1, err)
-	}
-	_, err = kv.Put(p2, nil)
-	if err != nil {
-		log.Fatalf("put %v: %v", p2, err)
-	}
-
 	got, meta, err := kv.Get("app/k1", nil)
 	if err != nil {
 		log.Fatalf("get: %v", err)
@@ -39,4 +36,32 @@ func main() {
 func newClient() (*api.Client, error) {
 	config := api.DefaultConfig()
 	return api.NewClient(config)
+}
+
+const defaultKeyPrefix = "app"
+
+func setupKV(c *api.Client) error {
+	pairs := []struct {
+		k string
+		v string
+	}{
+		{"k1", "v1"},
+		{"k2", "v2"},
+	}
+	for _, p := range pairs {
+		kv := &api.KVPair{
+			Key:   fmt.Sprintf("%s/%s", defaultKeyPrefix, p.k),
+			Value: []byte(p.v),
+		}
+		_, err := c.KV().Put(kv, nil)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func teardownKV(c *api.Client) error {
+	_, err := c.KV().DeleteTree(defaultKeyPrefix, nil)
+	return err
 }
